d223a8e5c70c08c2af398d86bf16b01f1cfe4fda
Adam Markowski <markowski.adam93@gmail.com>
2020-02-24 08:08:25 +0100

    Add first function to display result in div, add condition to except worng input value

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ const resultDiv = document.querySelector(".result-box");
 numberInput.addEventListener("input", (event) => {
     event.preventDefault();
 
-    console.log(event.target.value);
+    const inputNumber = Number(event.target.value);
 
+    // console.log(typeof inputNumber);
+
+    if (inputNumber < 0) {
+        return resultDiv.textContent = "Entered number is smaller than 0.";
+    }
+    if (!inputNumber) {
+        return resultDiv.textContent = "Wrong number format";
+    }
+
+    if (inputNumber === 0 || inputNumber == 1) {
+        return resultDiv.textContent = "No primary numbers here";
+    }
+
+    let resultArray = [];
+
+    primaryTest = (number) => {
+
+        for (let i = 1; i < number; i++) {
+            resultArray.push(i);
+        }
+
+        resultDiv.textContent = resultArray;
+    }
+
+    primaryTest(inputNumber);
 })
